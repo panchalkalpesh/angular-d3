1222382a7f5b9414cb6ec732c4801d53367bff0c
Kalpesh Panchal <panchalkalpesh@gmail.com>
2021-04-25 13:31:48 -0400

    Friends w/ Chip List added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-d3';
+  friends: string[] = [];
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  constructor() { }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.friends.push(value.trim());
+    }
+
+    if (input) { input.value = ''; }
+  }
+
+  remove(friend: string) {
+    this.friends = this.friends.filter(friend => friend !== friend);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,10 @@
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { NgModule } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule, MAT_CHIPS_DEFAULT_OPTIONS } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -9,9 +15,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatChipsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_CHIPS_DEFAULT_OPTIONS,
+      useValue: {
+        separatorKeyCodes: [ENTER, COMMA]
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
