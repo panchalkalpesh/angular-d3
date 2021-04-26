d7aa45159658c50d75052b1a3696072af916170b
Kalpesh Panchal <panchalkalpesh@gmail.com>
2021-04-25 21:54:59 -0400

    Link onSubmit with NgRx Action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ADD_FRIEND } from './friends.actions';
+import { Friend } from './friends.model';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +17,22 @@ export class AppComponent {
   friendsList: string[] = [];
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   myForm: FormGroup;
+  friends$: Observable<Friend[]>;
 
-  constructor(fb: FormBuilder) {
+  constructor(fb: FormBuilder, private store: Store<{ friends: Friend[] }>) {
     this.myForm = fb.group({
       'name': ['', Validators.required],
       'friends': [this.friendsList, Validators.required],
       'age': [null, Validators.required],
       'weight': [null, Validators.required]
     });
+    this.friends$ = store.select('friends');
   }
 
   onSubmit(form: FormGroup, formDirective: FormGroupDirective) {
     if (form.invalid || formDirective.invalid) { return; }
+    const { name, friends, age, weight } = form.value;
+    this.store.dispatch(ADD_FRIEND({ name, friends, age, weight }));
     this.friendsList = [];
     formDirective.resetForm(); // Angula Material checks the validity of FormGroupDirective and not the FormGroup
   }
